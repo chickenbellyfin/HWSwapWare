579c1450385fba8acbffa46ac5ad71bc3332cc90
Akshay <chickenbellyfinn@gmail.com>
2015-08-28 22:52:43 -0400

    Verify reddit usernames using heatware aliases

--- content_script.js
+++ content_script.js
@@ -1,45 +1,44 @@
 var flairs = document.getElementsByTagName("span");
 var count = 0;
 
+//find every flair on the page
 $('.flair').each(function(i, flair) {
-	//console.log("flair "+i);
-	if(/.+heatware\.com.+/.test($(flair).html())){
-		var heatwareURL = $(flair).html().replace("http://", "https://");
-		//console.log(heatwareURL);
 
+	//get the reddit username
+	var username = $(flair).parent().find(".author").first().html();
+
+	//check flair for a heatware URL
+	if(/.+heatware\.com.+/.test($(flair).html())){
+		var heatwareURL = $(flair).html().replace("http://", "https://"); //switch all heatware urls to HTTPS
+
+		//get the user's heatware page
 		$.ajax({
 			url:heatwareURL,
 			success:function(data){
 
 				var parser=new DOMParser();
-  				heatware = parser.parseFromString(data,"text/html");
+  				$heatware = $("<div/>").html(data).contents();
 
 				profile = {};
+				profile.verify = false;
 				profile.url = heatwareURL;
-				profile.pos = getNum("Positive Evals", heatware);
-				profile.neu = getNum("Neutral Evals", heatware);
-				profile.neg = getNum("Negative Evals", heatware);
-				replaceFlair(flair, profile);		
+				
+				//get # of pos/neu/neg evals
+				profile.pos = $heatware.find('[title="Positive Evals"] .num1').text();
+				profile.neu = $heatware.find('[title="Neutral Evals"] .num1').text();
+				profile.neg = $heatware.find('[title="Negative Evals"] .num1').text();
+
+				//check if reddit alias on heatware matches reddit username
+				$heatware.find('.subhead:contains(Aliases)').each(function(i, elem){
+					if($(elem).parent().find('strong:contains(Reddit)').first().parent().text().indexOf(username+" ") == 0){
+						profile.verify = true;
+					};					 
+				});
+
+				//replace flair with info
+				var hwstr = "Heatware: "+profile.pos+" / "+profile.neu+" / "+profile.neg + " ("+(profile.verify ? "confirmed" : "unconfirmed")+")";
+				$(flair).html("<a style='font-size:100% !important;font-weight:normal !important;' href='"+profile.url+"'>"+hwstr+"</a>");	
 			}
 		});
 	}	
 });
-
-function replaceFlair(flair, profile){
-	var hwstr = "Heatware: "+profile.pos+" / "+profile.neu+" / "+profile.neg;
-	$(flair).html("<a style='font-size:100% !important;font-weight:normal !important;' href='"+profile.url+"'>"+hwstr+"</a>");
-}
-
-
-function getNum(name, hw){
-	var nums = hw.getElementsByClassName("num");
-	for(var i = 0; i < nums.length; i++){
-		if(nums[i].title == name){
-			var num = nums[i].getElementsByClassName("num2")[0].innerHTML;
-			return num;
-		}
-	}	
-	return 0;
-}
-
-
