f5d4f5045b90cb1ffd691a1f325af4272798a168
Akshay Thejaswi <chickenbellyfinn@gmail.com>
2017-05-14 04:48:35 -0400

    update for heatware redesign

--- content_script.js
+++ content_script.js
@@ -10,6 +10,7 @@ $('.flair').each(function(i, flair) {
 	//check flair for a heatware URL
 	if(/.+heatware\.com.+/.test($(flair).html())){
 		var heatwareURL = $(flair).html().replace("http://", "https://"); //switch all heatware urls to HTTPS
+		heatwareURL = heatwareURL.replace("https://h", "https://www.h"); //www
 
 		//get the user's heatware page
 		$.ajax({
@@ -19,21 +20,30 @@ $('.flair').each(function(i, flair) {
 				var parser=new DOMParser();
   				$heatware = $("<div/>").html(data).contents();
 
-				profile = {};
+				var profile = {};
 				profile.verify = false;
 				profile.url = heatwareURL;
 				
 				//get # of pos/neu/neg evals
-				profile.pos = $heatware.find('[title="Positive Evals"] .num1').text();
-				profile.neu = $heatware.find('[title="Neutral Evals"] .num1').text();
-				profile.neg = $heatware.find('[title="Negative Evals"] .num1').text();
+				profile.pos = $heatware.find('[title="Positive Feedback"]+.f-26').text();
+				profile.neu = $heatware.find('[title="Neutral Feedback"]+.f-26').text();
+				profile.neg = $heatware.find('[title="Negative Feedback"]+.f-26').text();
 
 				//check if reddit alias on heatware matches reddit username
-				$heatware.find('.subhead:contains(Aliases)').each(function(i, elem){
-					if($(elem).parent().find('strong:contains(Reddit)').first().parent().text().indexOf(username+" ") == 0){
-						profile.verify = true;
-					};					 
+				var aliases = $heatware.find('.panel-heading:contains(Forum Aliases)').parent();
+				
+				aliases.find('.panel-body li:contains(on Reddit)').each(function(i, elem){
+						if($(elem).text().indexOf(username+" ") == 0){
+							profile.verify = true;
+						}
 				});
+
+				aliases.find('.panel-body li:contains(on /r/hardwareswap)').each(function(i, elem){
+						if($(elem).text().indexOf(username+" ") == 0){
+							profile.verify = true;
+						}
+				});
+
 
 				//replace flair with info
 				var hwstr = "Heatware: "+profile.pos+" / "+profile.neu+" / "+profile.neg + " ("+(profile.verify ? "confirmed" : "unconfirmed")+")";
